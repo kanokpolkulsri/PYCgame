9ce016b23ad9faadf48505aa790a473a94b568fe
tonplamm <tonplamm@Kanokporns-MacBook-Air.local>
2016-12-03 14:21:04 +0700

    character changes when scores change (+)

--- calculate.py
+++ calculate.py
@@ -2,8 +2,10 @@ import arcade
 class cal:
     ans_chai = 0
     ans_pa = 0
-    score_chai = 1
-    score_pa = 1
+    score_chai = 0
+    score_pa = 0
+    people_chai = arcade.Sprite('images/cha2.png')
+    people_pa = arcade.Sprite('images/pa2.png')
 
     def on_key_press(self, key, key_modifiers):
         if key == arcade.key.A:
@@ -18,7 +20,8 @@ class cal:
             self.ans_pa = 2
         elif key == arcade.key.L:
             self.ans_pa = 3
-        print(self.score_chai, self.score_pa)
+        if key == arcade.key.SPACE:
+            print(self.score_chai, self.score_pa)
 
     def update_result(self):
         if self.ans_chai == 1 and self.ans_pa == 1:
@@ -58,3 +61,32 @@ class cal:
             self.ans_chai = 0
             self.ans_pa = 0
             self.score_chai += 1
+
+    def chai(self):
+        if self.score_chai - self.score_pa == 1:
+            self.people_chai = arcade.Sprite('images/cha2.png')
+        if self.score_chai - self.score_pa == 2:
+            self.people_chai = arcade.Sprite('images/cha4.png')
+        if self.score_chai - self.score_pa == 3:
+            self.people_chai = arcade.Sprite('images/cha4.png')
+        if self.score_chai - self.score_pa == 4:
+            self.people_chai = arcade.Sprite('images/cha5.png')
+        if self.score_chai - self.score_pa == 5:
+            self.people_chai = arcade.Sprite('images/cha5.png')
+        self.people_chai.set_position(100, 100)
+        self.people_chai.draw()
+
+    def pa(self):
+        if self.score_pa - self.score_chai == 1:
+            self.people_pa = arcade.Sprite('images/pa2.png')
+        if self.score_pa - self.score_chai == 2:
+            self.people_pa = arcade.Sprite('images/pa4.png')
+        if self.score_pa - self.score_chai == 3:
+            self.people_pa = arcade.Sprite('images/pa4.png')
+        if self.score_pa - self.score_chai == 4:
+            self.people_pa = arcade.Sprite('images/pa5.png')
+        if self.score_pa - self.score_chai == 5:
+            self.people_pa = arcade.Sprite('images/pa5.png')
+        self.people_pa.set_position(900, 100)
+        self.people_pa.draw()
+
--- screen.py
+++ screen.py
@@ -10,26 +10,18 @@ class SpaceGameWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
         arcade.set_background_color(arcade.color.BLACK)
-        self.man = arcade.Sprite('images/cha2.png')
-        self.man.set_position(100,100)
         self.cal = cal()
 
     def on_draw(self):
         arcade.start_render()
-        self.man.draw()
+        self.cal.chai()
+        self.cal.pa()
+        arcade.draw_text(str(self.cal.score_chai), 30, self.height - 30, arcade.color.WHITE, 20)
+        arcade.draw_text(str(self.cal.score_pa), self.width - 30, self.height - 30, arcade.color.WHITE, 20)
 
     def on_key_press(self, key, key_modifiers):
         self.cal.update_result()
         self.cal.on_key_press(key, key_modifiers)
-    #     if key == arcade.key.A:
-    #         self.man = arcade.Sprite('images/cha1.png')
-    #         self.man.set_position(100, 100)
-    #     elif key == arcade.key.S:
-    #         self.man = arcade.Sprite('images/cha3.png')
-    #         self.man.set_position(100, 100)
-    #     elif key == arcade.key.D:
-    #         self.man = arcade.Sprite('images/cha4.png')
-    #         self.man.set_position(100, 100)
 
 if __name__ == '__main__':
     window = SpaceGameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
